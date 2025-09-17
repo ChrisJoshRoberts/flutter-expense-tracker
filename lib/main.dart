91522c81b8b5b7099cc15f232107743bd5f384ef
ChrisJoshRoberts <chrisrobertscollective@gmail.com>
2025-09-17 15:19:40 +0200

    updating color scheme

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,18 @@ void main() {
       home: Expenses(),
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white, 
+        primarySwatch: Colors.amber,
+        dialogTheme: DialogThemeData(backgroundColor: Colors.white),
+        filledButtonTheme: FilledButtonThemeData(
+          style: FilledButton.styleFrom(
+            backgroundColor: Colors.amber[900],
+          ),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.amber[900],
+          ),
+        ),
       ),
       darkTheme: ThemeData(
         scaffoldBackgroundColor: Colors.grey[900],
--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,9 +10,14 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enteredTitle = '';
-  void _saveTitleInput(String inputValue) {
-    _enteredTitle = inputValue;
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
   }
 
   @override
@@ -22,19 +27,38 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
-            onChanged: _saveTitleInput,
+            controller: _titleController,
             maxLength: 50,
             decoration: InputDecoration(labelText: 'Title'),
           ),
           Row(
             children: [
-              ElevatedButton(
-                onPressed: () {
-                  print(_enteredTitle);
-                },
-                child: Text('Save Expense'),
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  decoration: InputDecoration(labelText: 'Amount'),
+                ),
               ),
             ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: Row(
+              children: [
+                FilledButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                    print(_amountController.text);
+                  },
+                  child: Text('Save Expense'),
+                ),
+                SizedBox(width: 16),
+                TextButton(onPressed: () {
+                  Navigator.pop(context);
+                }, child: Text('Cancel')),
+              ],
+            ),
           ),
         ],
       ),
