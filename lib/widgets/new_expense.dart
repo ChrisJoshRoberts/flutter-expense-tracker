0c0dbb421e340ba10530635eede0531a0769b393
ChrisJoshRoberts <chrisrobertscollective@gmail.com>
2025-09-18 11:26:40 +0200

    adding dropdownMenu

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart' as expense_model;
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -12,6 +14,21 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+  var _SelectedCategory;
+
+  void presentDatePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now().subtract(Duration(days: 365)),
+      lastDate: DateTime.now(),
+    ).then((value) {
+      setState(() {
+        _selectedDate = value;
+      });
+    });
+  }
 
   @override
   void dispose() {
@@ -43,21 +60,53 @@ class _NewExpenseState extends State<NewExpense> {
                   ),
                 ),
               ),
-              SizedBox(width: 16),
+              SizedBox(width: 8),
               Expanded(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text('Selected Date'),
+                    Text(
+                      style: TextStyle(
+                        color: _selectedDate == null
+                            ? const Color.fromARGB(255, 86, 86, 86)
+                            : Colors.amber[900],
+                        fontWeight: FontWeight.bold,
+                      ),
+                      _selectedDate == null
+                          ? 'Selected Date'
+                          : '${_selectedDate?.day.toString().padLeft(2, '0')}/${_selectedDate?.month.toString().padLeft(2, '0')}/${_selectedDate?.year.toString()}',
+                    ),
                     IconButton(
-                      onPressed: () {},
+                      onPressed: presentDatePicker,
                       icon: Icon(Icons.calendar_month),
                     ),
                   ],
                 ),
               ),
             ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                DropdownButton<expense_model.Category>(
+                  value: _SelectedCategory,
+                  hint: Text('Select Category'),
+                  items: expense_model.Category.values.map((item) {
+                    return DropdownMenuItem<expense_model.Category>(
+                      value: item,
+                      child: Text(item.name),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      _SelectedCategory = value;
+                    });
+                  },
+                ),
+              ],
+            ),
           ),
           Padding(
             padding: const EdgeInsets.only(top: 16),
