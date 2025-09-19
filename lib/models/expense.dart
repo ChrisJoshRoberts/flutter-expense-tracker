9f103a54884e7d67ca57167bb645b19a2721c303
ChrisJoshRoberts <chrisrobertscollective@gmail.com>
2025-09-19 14:41:13 +0200

    adding Expense Bucket

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -26,3 +26,22 @@ class Expense {
     required this.category,
   }) : id = uuid.v4();
 }
+
+class ExpenseBucket {
+  final Category category;
+  final List<Expense> expenses;
+  const ExpenseBucket(this.category, this.expenses);
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+    : expenses = allExpenses
+          .where((expense) => expense.category == category)
+          .toList();
+
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
+}
